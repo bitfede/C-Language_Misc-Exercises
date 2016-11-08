d801bd912eb62542ef1ab99d48afed89f6ee6412
Federico Giorgio De Faveri <f.g.defaveri@gmail.com>
2016-11-08 15:02:21 -0500

    created labtask9 and started laying down struct and enum

---
+++ LabTask9/jungleExploration.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//------------------------------------
+
+#define MAXSTRLEN 128
+
+//-----------------------------------
+
+typedef enum {
+  mammal,
+  bird,
+  fish,
+  insect,
+  error
+} AnimalType;
+
+struct specie {
+  char* name;
+  float size;
+  AnimalType type;
+};
+
+
+
+int main() {
+  
+  //create array of structs
+
+
+
+
+
+
+  return(EXIT_SUCCESS);
+}
